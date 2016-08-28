bef9132ee4134a03ec89a21b3327cc9f6b94bc1b
Sean Auriti <sean@alphaonelabs.com>
2016-08-28 17:30:54 -0400

    Facebook login closes #3

--- bugheist/settings.py
+++ bugheist/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = (
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'allauth.socialaccount.providers.github',
+    'allauth.socialaccount.providers.facebook',
     'actstream',
     'django_gravatar',
 )
