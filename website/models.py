a1a9efe9cacbfa33590bbc5a15e73b611c017a4e
Conrad Kroone <ConradKroone@gmail.com>
2016-08-28 15:49:45 -0400

    Setup screenshot uploading closes #11

--- website/models.py
+++ website/models.py
@@ -9,7 +9,7 @@ class Issue(models.Model):
     user = models.ForeignKey(User)
     url = models.URLField()
     description = models.TextField()
-    screenshot = models.ImageField(null=True, blank=True)
+    screenshot = models.ImageField(null=True, blank=True, upload_to="screenshots")
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
 
