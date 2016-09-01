d87887aecf7fc67b4ad4baa76eb846cb673997dc
Sean Auriti <sean@alphaonelabs.com>
2016-08-31 23:45:01 -0400

    Adds leaderboard

--- website/admin.py
+++ website/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from website.models import Issue
+from website.models import Issue, Points
 
 class IssueAdmin(admin.ModelAdmin):
     list_display = ('user','url','description','screenshot','created','modified')
 admin.site.register(Issue, IssueAdmin)
+admin.site.register(Points)
