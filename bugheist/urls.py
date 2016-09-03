f2a8accd0eeb31939c6a5d2d2d8cc00c57320aeb
Sean Auriti <sean@alphaonelabs.com>
2016-09-02 23:15:05 -0400

    tweaks

--- bugheist/urls.py
+++ bugheist/urls.py
@@ -14,7 +14,7 @@ import website.views
 urlpatterns = patterns('',
     url(r'^$', website.views.index, name='index'),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^issue/(?P<slug>\w+)/$', IssueView.as_view(), name="issue_view"),
+    url(r'^issue/(?P<slug>\w+)$', IssueView.as_view(), name="issue_view"),
     url(r'^issue/', login_required(IssueCreate.as_view()), name="issue"),
     url(r'^upload/(?P<time>[^/]+)/(?P<hash>[^/]+)/', UploadCreate.as_view(), name="upload"),
     url(r'^profile/(?P<slug>[^/]+)/$', UserProfileDetailView.as_view(), name="profile"),
