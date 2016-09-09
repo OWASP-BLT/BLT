32d289fa6f3e72632e9d9e42ac3b6915ae240ca2
Sean Auriti <sean@alphaonelabs.com>
2016-09-09 18:17:14 -0400

    tweak hunt list view and change button to get started.

--- website/admin.py
+++ website/admin.py
@@ -4,6 +4,10 @@ from website.models import Issue, Points, Hunt
 
 class IssueAdmin(admin.ModelAdmin):
     list_display = ('user','url','description','screenshot','created','modified')
+
+class HuntAdmin(admin.ModelAdmin):
+    list_display = ('user','url','prize','logo','plan','created','modified')
+
 admin.site.register(Issue, IssueAdmin)
 admin.site.register(Points)
-admin.site.register(Hunt)
+admin.site.register(Hunt, HuntAdmin)
