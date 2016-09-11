9b360c886f55f08c461f2f1c33f9ff54d228e62f
Sean Auriti <sean@alphaonelabs.com>
2016-09-11 01:27:11 -0400

    add date joined to user list

--- website/admin.py
+++ website/admin.py
@@ -1,12 +1,21 @@
 from django.contrib import admin
 
 from website.models import Issue, Points, Hunt
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
 
 class IssueAdmin(admin.ModelAdmin):
     list_display = ('user','url','description','screenshot','created','modified')
 
 class HuntAdmin(admin.ModelAdmin):
     list_display = ('user','url','prize','logo','plan','created','modified')
+
+
+admin.site.unregister(User)
+
+UserAdmin.list_display = ('id','username','email', 'first_name', 'last_name', 'is_active', 'date_joined', 'is_staff')
+    
+admin.site.register(User, UserAdmin)
 
 admin.site.register(Issue, IssueAdmin)
 admin.site.register(Points)
