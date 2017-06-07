85bf6c81a755909d6a4ba7159da8482330c4bbf3
Neethu Mariya Joy <Roboneet@users.noreply.github.com>
2017-06-08 05:02:28 +0530

    fix issue 240 (#244)
    * changed front-page boxes
    
    * changed box sizes
    
    * working text slider
    
    * added styles
    
    * responsive slider

---
+++ website/static/js/text-slider.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+			var lines = $('.hero .text-slider-line');
+			var n = lines.length;
+			var i = 0;
+			$(lines[0]).fadeIn();
+			var changeText = setInterval(function(){
+				console.log(i);
+				console.log(lines);
+				console.log(lines[i].className);
+				$(lines[i]).fadeOut();
+				if(i==(n-1)){
+					i=0;
+				}
+				else{
+					i++;
+				}
+				setTimeout(function(){
+				$(lines[i]).fadeIn();
+				},500);
+			},2000);
+
+
+});
