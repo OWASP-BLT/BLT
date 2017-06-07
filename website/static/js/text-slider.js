9db5c713798ca7ad414ba88522a186f3c3c75ac9
Roboneet <neethumariyajoy@gmail.com>
2017-06-08 04:49:13 +0530

    added styles

--- website/static/js/text-slider.js
+++ website/static/js/text-slider.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
 			var lines = $('.hero .text-slider-line');
-			var i = 2;
-			var n = lines.length;	
+			var n = lines.length;
+			var i = 0;
+			$(lines[0]).fadeIn();
 			var changeText = setInterval(function(){
 				console.log(i);
 				console.log(lines);
@@ -13,35 +14,10 @@ $(document).ready(function(){
 				else{
 					i++;
 				}
+				setTimeout(function(){
 				$(lines[i]).fadeIn();
+				},500);
 			},2000);
 
 
-		// var change = setInterval(function(){
-		// 	// lines.each(function(index, ele){
-		// 	// 	console.log(ele)
-		// 	// 	var k = setTimeout(function(){
-		// 	// 	$(ele).fadeOut();
-		// 	// 	console.log(ele)},1000)
-
-		// 	// 	$(ele).next().fadeIn();
-
-		// 	// }
-		// 	// 	)
-		// 	var i = 0;
-		// 	var l = lines.length;
-		// 	var k = setInterval(function(){
-		// 		console.log(i);
-		// 		console.log(lines);
-		// 		console.log(lines[i].className);
-		// 		$(lines[i]).fadeOut();
-		// 		if(i==(n-1)){
-		// 			clearInterval(k)
-		// 		}
-		// 		else{
-		// 			i++;
-		// 		}
-		// 		$(lines[i]).fadeIn();
-		// 	},1000)
-		// },4000)
 });
