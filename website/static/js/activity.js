7694d47d3a5ecd3a31065787f2be613c68a17ef9
Roboneet <neethumariyajoy@gmail.com>
2017-06-12 01:37:41 +0530

    change in height and code style

--- website/static/js/activity.js
+++ website/static/js/activity.js
@@ -1,35 +1,21 @@
 $(document).ready(function(){
-	
 	$('.text-trunc').each(function(index, ele){
-		console.log(ele)
-
-		var text = $(ele).siblings('.small').text().trim() //.substring(0, 30).split(" ").slice(0, -1).join(" ") + "...";
-		console.log(text)
+		var text = $(ele).siblings('.small').text().trim() 
 		var actor = /^[^\s]+/.exec(text)
-		console.log(actor[0])
 		var rest = text.slice(actor[0].length).trim().substring(0, 20).split(" ").slice(0, -1).join(" ") + "...";
 		$(ele).text(actor[0]+ ' '+rest);
 	})
-
-
-
-
 
 	$('.more_info').click(function(){
 		if($(this)[0]==$('.open')[0]){
 			close_panel()
 			return;
 		}
-		// console.log($(this))
-		// console.log($('.open'))
-
-		close_panel()
-		
-		
+        close_panel()
 		$(this).html("<a>See less</a>")
-		$(this).parent().animate({'height':'400px'})
-		$(this).parent().parent().animate({'height': '400px'})
-		$(this).parent().parent().parent().animate({'height': '400px'})
+		$(this).parent().animate({'height':'200px'})
+		$(this).parent().parent().animate({'height': '200px'})
+		$(this).parent().parent().parent().animate({'height': '200px'})
 		$(this).siblings('.text-trunc').hide()
 		$(this).siblings('.small').show()
 		$(this).addClass('open')
@@ -37,14 +23,11 @@ $(document).ready(function(){
 
 	function close_panel(){
 		$('.open').html('<a>See more</a>')
-		$('.open').parent().animate({'height':'200px'})
-		$('.open').parent().parent().animate({'height': '200px'})
-		$('.open').parent().parent().parent().animate({'height': '200px'})
+		$('.open').parent().animate({'height':'160px'})
+		$('.open').parent().parent().animate({'height': '160px'})
+		$('.open').parent().parent().parent().animate({'height': '160px'})
 		$('.open').siblings('.small').hide()
 		$('.open').siblings('.text-trunc').show()
-		$('.open').removeClass('open')
-		
+		$('.open').removeClass('open')	
 	}
-
-
 })
