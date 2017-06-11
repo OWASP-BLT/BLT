cb65f6b05871ac479b58053614804aba6ddac8a5
Roboneet <neethumariyajoy@gmail.com>
2017-06-12 00:48:41 +0530

    added truncated charectors

--- website/static/js/activity.js
+++ website/static/js/activity.js
@@ -1,12 +1,27 @@
 $(document).ready(function(){
 	
+	$('.text-trunc').each(function(index, ele){
+		console.log(ele)
+
+		var text = $(ele).siblings('.small').text().trim() //.substring(0, 30).split(" ").slice(0, -1).join(" ") + "...";
+		console.log(text)
+		var actor = /^[^\s]+/.exec(text)
+		console.log(actor[0])
+		var rest = text.slice(actor[0].length).trim().substring(0, 20).split(" ").slice(0, -1).join(" ") + "...";
+		$(ele).text(actor[0]+ ' '+rest);
+	})
+
+
+
+
+
 	$('.more_info').click(function(){
 		if($(this)[0]==$('.open')[0]){
 			close_panel()
 			return;
 		}
-		console.log($(this))
-		console.log($('.open'))
+		// console.log($(this))
+		// console.log($('.open'))
 
 		close_panel()
 		
@@ -15,6 +30,7 @@ $(document).ready(function(){
 		$(this).parent().animate({'height':'400px'})
 		$(this).parent().parent().animate({'height': '400px'})
 		$(this).parent().parent().parent().animate({'height': '400px'})
+		$(this).siblings('.text-trunc').hide()
 		$(this).siblings('.small').show()
 		$(this).addClass('open')
 	})
@@ -25,7 +41,10 @@ $(document).ready(function(){
 		$('.open').parent().parent().animate({'height': '200px'})
 		$('.open').parent().parent().parent().animate({'height': '200px'})
 		$('.open').siblings('.small').hide()
+		$('.open').siblings('.text-trunc').show()
 		$('.open').removeClass('open')
 		
 	}
+
+
 })
