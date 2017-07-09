7e0aec8c017ba102c0b7e7468848e4a0283cd6ef
Sean Auriti <sean@alphaonelabs.com>
2017-07-08 21:02:52 -0400

    add comments to admin

--- comments/admin.py
+++ comments/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from models import Comment
+
+class MyCommentsAdmin(admin.ModelAdmin):
+	list_display = ('id','author', 'issue', 'text', 'created_date')
+
+admin.site.register(Comment, MyCommentsAdmin)
