682e478726b39b4323a5876b39dbc7e063f86b0b
Sourav Badami <souravbadami@gmail.com>
2017-07-11 18:47:19 +0530

    Integrated i18n support and added french language support to BLT.

--- bugheist/urls.py
+++ bugheist/urls.py
@@ -56,6 +56,7 @@ urlpatterns = patterns('',
     url(r'^social/$', TemplateView.as_view(template_name="social.html")),
     url(r'^search/$', website.views.search),
     url(r'^report/$', TemplateView.as_view(template_name="report.html")),
+    url(r'^i18n/', include('django.conf.urls.i18n')),
     
     
 
