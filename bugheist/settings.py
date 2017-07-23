99c14bb5e2991b25ca8062beadb248c3c27c2242
Sourav Badami <souravbadami@gmail.com>
2017-07-24 04:23:43 +0530

    Added debug toolbar for debugging. #428

--- bugheist/settings.py
+++ bugheist/settings.py
@@ -10,9 +10,10 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 """
 
 import os
+import sys
+
 import dj_database_url
 from django.http import Http404
-import sys
 from django.utils.translation import ugettext_lazy as _
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -54,9 +55,31 @@ INSTALLED_APPS = (
     'email_obfuscator',
     'import_export',
     'comments',
+    'debug_toolbar',
 )
 
+DEBUG_TOOLBAR_PANELS = [
+    'debug_toolbar.panels.versions.VersionsPanel',
+    'debug_toolbar.panels.timer.TimerPanel',
+    'debug_toolbar.panels.settings.SettingsPanel',
+    'debug_toolbar.panels.headers.HeadersPanel',
+    'debug_toolbar.panels.request.RequestPanel',
+    'debug_toolbar.panels.sql.SQLPanel',
+    'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+    'debug_toolbar.panels.templates.TemplatesPanel',
+    'debug_toolbar.panels.cache.CachePanel',
+    'debug_toolbar.panels.signals.SignalsPanel',
+    'debug_toolbar.panels.logging.LoggingPanel',
+    'debug_toolbar.panels.redirects.RedirectsPanel',
+]
+
+DEBUG_TOOLBAR_CONFIG = {
+    'INTERCEPT_REDIRECTS': False,
+    "SHOW_TOOLBAR_CALLBACK": lambda request: True,
+}
+
 MIDDLEWARE_CLASSES = (
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
