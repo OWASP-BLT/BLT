08e231db4a974d70ac195f5c68177c31a4e65418
Mohit Anand <anand.mohit001@gmail.com>
2017-07-23 12:43:06 +0530

    Added async edit-comment feature

--- website/static/js/issue.js
+++ website/static/js/issue.js
@@ -33,3 +33,90 @@ $(function () {
         $.notify('Copied!', {style: "custom", className: "success"});
     });
 });
+
+    $(document).on('submit','#comments',function(e){
+        e.preventDefault();
+        $.ajax({
+            type: 'POST',
+            url: '/issue/comment/add/',
+            data:{
+                text_comment: $('#text_comment').val(),
+                issue_pk: $('#issue_pk').val(),
+                csrfmiddlewaretoken: $('#comments input[name=csrfmiddlewaretoken]').val(),
+            },
+            success: function(data){
+                 $('#target_div').html(data);
+                 $('#text_comment').val('');
+            }
+        });
+    });
+
+    $('body').on('click', '.del_comment', function (e){
+        e.preventDefault();
+        if(confirm("Delete this comment?")==true){
+            $.ajax({
+                type: 'POST',
+                url: "/issue/comment/delete/",
+                data:{
+                    comment_pk:$(this).attr('name'),
+                    issue_pk: $('#issue_pk').val(),
+                    csrfmiddlewaretoken: $('#comments input[name=csrfmiddlewaretoken]').val(),
+                },
+                success: function(data) {
+                    $('#target_div').html(data);
+                },
+            });
+        }
+    });
+
+
+    $('body').on('click', '.edit_comment', function (e){
+        e.preventDefault();
+        $('#edit_form_'+$(this).attr('name')).attr('style','');
+        var comment_id = $(this).attr('name');
+        var old_message = $('#text_div_'+comment_id).text();
+        $('#text_comment_'+comment_id).val(old_message);
+        $('#text_div_'+comment_id).text('');
+    });
+
+    $(document).on('submit','.edit_form',function(f){
+        f.preventDefault();
+        var comment_id = $(this).attr('name');
+        var issue_pk = $('#text_div_'+comment_id).text();
+        var old_message = $('#text_div_'+comment_id).text();
+
+        $.ajax({
+            type: 'GET',
+            url: '/issue/'+ $('#issue_pk').val()+ '/comment/edit/',
+            data:{
+                comment_pk: comment_id,
+                text_comment: $('#text_comment_'+comment_id).val() ,
+                issue_pk: $('#issue_pk').val(),
+            },
+            success: function(data){
+                 $('#target_div').html(data);
+            }
+
+        });
+    });
+
+
+    function ajax_complete() {
+        var search = $('#text_comment').val();
+        var data = { search: search };
+        $.ajax({
+            type: 'GET',
+            url: '/comment/autocomplete/',
+            data: data,
+            dataType: 'jsonp',
+            jsonp: 'callback',
+            jsonpCallback: 'ajax_render',
+        });
+    }
+
+    function ajax_render(data) {
+        $('#text_comment').atwho({
+            at: "@",
+            data:data
+        });
+    }
