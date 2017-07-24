d4296aef13d8788d8d9dfb1cceb06b37a5ed079d
Sourav Badami <souravbadami@gmail.com>
2017-07-24 05:37:43 +0530

    Minor changes.

--- bugheist/urls.py
+++ bugheist/urls.py
@@ -68,4 +68,6 @@ urlpatterns = [
 if settings.DEBUG:
     import debug_toolbar
 
-    urlpatterns += [url(r'^__debug__/', include(debug_toolbar.urls))]
+    urlpatterns = [
+                      url(r'^__debug__/', include(debug_toolbar.urls)),
+                  ] + urlpatterns
