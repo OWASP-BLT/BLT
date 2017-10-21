f87c09a4b10f468cd48a9cdc9f7fb5fe2490624f
rtgdk <rohit.lodhartg@gmail.com>
2017-10-21 13:50:06 +0530

    Comment out whitenoise storage location to run collectstatic without error. Signed-off-by: rtgdk <rohit.lodhartg@gmail.com>

--- bugheist/settings.py
+++ bugheist/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    #'django.contrib.staticfiles',
     'django.contrib.sites',
     'django.contrib.humanize',
     'website',
