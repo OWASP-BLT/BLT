0c9b2ff85c3c14b97354dcdd14affeaa5b494a8a
srahulbadami <srahulbadami@gmail.com>
2018-07-21 01:38:48 +0530

    Updated Serializers for Mobile Scoreboard

--- website/serializers.py
+++ website/serializers.py
@@ -2,7 +2,6 @@ from website.models import Issue, User , UserProfile,Points, Domain
 from rest_framework import routers, serializers, viewsets, filters
 import django_filters
 
-
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
@@ -49,13 +48,9 @@ class DomainViewSet(viewsets.ModelViewSet):
     filter_backends = (filters.SearchFilter,)
     search_fields = ('url', 'name')
 
-class ScoreBoardViewSet(viewsets.ModelViewSet):
-    serializer_class = DomainSerializer
-    queryset = Domain.objects.all().order_by('-modified')
 
 router = routers.DefaultRouter()
 router.register(r'issues', IssueViewSet, base_name="issues")
 router.register(r'userissues', UserIssueViewSet, base_name="userissues")
 router.register(r'profile', UserProfileViewSet, base_name="profile")
 router.register(r'domain', DomainViewSet, base_name="domain")
-router.register(r'scoreboard', ScoreBoardViewSet, base_name="scoreboard")
