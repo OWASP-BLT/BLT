555dc4d568cd3276ace5d860b22097e79318fb12
S Rahul Badami <srahulbadami@gmail.com>
2020-05-26 01:14:17 +0530

    Removed whitenoise nonstatic for debug mode

--- bugheist/settings.py
+++ bugheist/settings.py
@@ -34,7 +34,6 @@ SITE_ID = 1
 # Application definition
 
 INSTALLED_APPS = (
-    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
