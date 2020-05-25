db38718ddbdf3041a8abdb6d1f34f8b56b1447c9
S Rahul Badami <srahulbadami@gmail.com>
2020-05-26 01:04:02 +0530

    Updated the packages to current version (#651)

--- bugheist/wsgi.py
+++ bugheist/wsgi.py
@@ -12,7 +12,7 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bugheist.settings")
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
+from whitenoise import WhiteNoise
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+application = WhiteNoise(application)
