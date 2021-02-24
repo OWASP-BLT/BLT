54bcdd48906bd43f03c48196447355e3ae19cb9b
Samuel Rosado <SamuelLRosado@jourrapide.com>
2021-02-23 22:52:14 -0500

    Had to revert some GSOC code as it made the homepage unusable.  created website-temp of the changes that need to be cleaned up and merged back in.

--- website/static/js/embed.js
+++ website/static/js/embed.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     var button = document.createElement("Button");
-    button.style = "bottom:15px;left:15px;position:fixed;z-index: 12;border-radius:100%;background: url('https://www.bugheist.com/static/img/logo.0cc160e97934.png') no-repeat center; height: 50px; width: 50px; outline: none;background-size: 50px 50px;"
+    button.style = "bottom:15px;left:15px;position:fixed;z-index: 12;border-radius:100%;background-color:white; background: white url('https://www.bugheist.com/static/img/logo.png') no-repeat center; height: 50px; width: 50px; outline: none;background-size: 30px 30px;"
     document.body.appendChild(button);
     var url = window.location.href;
     var bugheist = 'https://www.bugheist.com/report/?url=' + url;
