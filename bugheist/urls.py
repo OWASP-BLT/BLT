fdc04effe785006acebd85728b889d7f20c87639
Ankit Choudhary <ankitchoudhary202.ac@gmail.com>
2021-03-31 13:58:50 +0530

    Add Swagger documentation (#718)

--- bugheist/urls.py
+++ bugheist/urls.py
@@ -18,12 +18,28 @@ from website.views import (UserProfileDetailView, IssueCreate, UploadCreate, Ema
                            CreateHunt, DraftHunts, UpcomingHunts, CompanySettings, OngoingHunts, PreviousHunts,
                            DomainList, UserProfileDetailsView, JoinCompany )
 from rest_framework.authtoken import views
-
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 favicon_view = RedirectView.as_view(url='/static/favicon.ico', permanent=True)
 
 admin.autodiscover()
-
+schema_view = get_schema_view(
+   openapi.Info(
+      title="BugHeist API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 urlpatterns = [
+                  url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+                  url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+                  url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
                   url(r'^$', website.views.index, name='index'),
                   url(r'^dashboard/company/$', website.views.company_dashboard, name='company_dashboar_home'),
                   url(r'^dashboard/user/profile/addbalance$', website.views.addbalance, name='addbalance'),
