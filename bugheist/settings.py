6eea8ac5542eefdc78cf65aea0e7d4f4b58d0c5b
ankit2001 <ankitchoudhary202.ac@gmail.com>
2021-07-28 11:12:05 +0530

    Add Rest APIs for about, policy and terms page

--- bugheist/settings.py
+++ bugheist/settings.py
@@ -272,7 +272,7 @@ ACCOUNT_EMAIL_VERIFICATION = "optional"
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
 # Allow all host headers
-ALLOWED_HOSTS = [".bugheist.com", "127.0.0.1", "localhost", "bugheist-staging.herokuapp.com"]
+ALLOWED_HOSTS = [".bugheist.com", "127.0.0.1", "localhost", "bugheist-staging.herokuapp.com", "b9940a443045.ngrok.io"]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
