373bca4e60152be4f0ae95ca3a1722bdbe2b0321
Samuel Rosado <SamuelLRosado@jourrapide.com>
2023-02-20 03:39:44 -0500

    Delete bugheist directory

--- bugheist/wsgi.py
+++
@@ -1,9 +0,0 @@
-import os
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blt.settings")
-
-from django.core.wsgi import get_wsgi_application
-from whitenoise import WhiteNoise
-
-application = get_wsgi_application()
-application = WhiteNoise(application)
