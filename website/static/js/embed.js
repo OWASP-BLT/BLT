3e1af192482c68ce9188fb6eaeee0dda8d20e68e
fredfalcon <7475382+fredfalcon@users.noreply.github.com>
2023-02-28 16:15:36 -0500

    update the embed script to use a dynamic variable

--- website/static/js/embed.js
+++ website/static/js/embed.js
@@ -1,9 +1,11 @@
 $(document).ready(function () {
+    // change this to be the server where blt is hosted
+    var fqdn = "https://www.hosted-blt-server.com";
     var button = document.createElement("Button");
-    button.style = "bottom:15px;left:15px;position:fixed;z-index: 12;border-radius:100%;background-color:white; background: white url('https://www.bugheist.com/static/img/logo.png') no-repeat center; height: 50px; width: 50px; outline: none;background-size: 30px 30px;"
+    button.style = "bottom:15px;left:15px;position:fixed;z-index: 12;border-radius:100%;background-color:white; background: white url('" + fqdn + "') no-repeat center; height: 50px; width: 50px; outline: none;background-size: 30px 30px;"
     document.body.appendChild(button);
     var url = window.location.href;
-    var redirect_url = 'https://www.bugheist.com/report/?url=' + url;
+    var redirect_url = fqdn + url;
     button.onclick = function () {
         var redirectWindow = window.open(redirect_url, '_blank');
         redirectWindow.location;
