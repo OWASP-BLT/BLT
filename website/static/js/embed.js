9fa31ea3027d36ab4acccbc6167623446b18d007
Samuel Rosado <SamuelLRosado@jourrapide.com>
2023-02-28 16:12:36 -0500

    a few more name fixes

--- website/static/js/embed.js
+++ website/static/js/embed.js
@@ -3,9 +3,9 @@ $(document).ready(function () {
     button.style = "bottom:15px;left:15px;position:fixed;z-index: 12;border-radius:100%;background-color:white; background: white url('https://www.bugheist.com/static/img/logo.png') no-repeat center; height: 50px; width: 50px; outline: none;background-size: 30px 30px;"
     document.body.appendChild(button);
     var url = window.location.href;
-    var bugheist = 'https://www.bugheist.com/report/?url=' + url;
+    var redirect_url = 'https://www.bugheist.com/report/?url=' + url;
     button.onclick = function () {
-        var redirectWindow = window.open(bugheist, '_blank');
+        var redirectWindow = window.open(redirect_url, '_blank');
         redirectWindow.location;
     }
 });
