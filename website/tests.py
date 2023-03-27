04a3d68f33df5bf40e1127f41cc0ac64eae27050
Pranav Arya <pranav_a@ece.iitr.ac.in>
2023-03-27 19:45:00 +0530

    Created a method to hide image from public when issue is hidden (#1130)
    * image rename on hide
    
    * delete from google cloude picture to be hidden
    
    * updated dependencies
    
    * wrote test for on hide signal
    
    * updated last 6 lines of the test can be rewritten to assert true
    
    * fixed remove try except
    
    * fixed remove unsused fields
    
    * file rename test and undo formatting
    
    * removed extra spaces
    
    ---------
    
    Co-authored-by: swapnil shinde <swapnilshinde9382@gmail.com>

--- website/tests.py
+++ website/tests.py
@@ -1,32 +1,33 @@
+from .models import Issue, IssueScreenshot
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.core.files.storage import default_storage
+from django.test import TestCase
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium import webdriver
+from django.test.utils import override_settings
 import os
-import time
-import sys
 
 from django.test import LiveServerTestCase
 
 os.environ['DJANGO_LIVE_TEST_SERVER_ADDRESS'] = 'localhost:8082'
-
-from django.test.utils import override_settings
-from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 class MySeleniumTests(LiveServerTestCase):
     fixtures = ['initial_data.json']
 
     @classmethod
     def setUpClass(cls):
-        print (sys.path)
         d = DesiredCapabilities.CHROME
         d["loggingPrefs"] = {"browser": "ALL"}
         option = webdriver.ChromeOptions()
         option.add_argument("window-size=1920,1080")
 
         # switch these
-        cls.selenium = webdriver.Chrome(ChromeDriverManager().install(), desired_capabilities=d, options=option)
+        cls.selenium = webdriver.Chrome(ChromeDriverManager(
+        ).install(), desired_capabilities=d, options=option)
         super(MySeleniumTests, cls).setUpClass()
 
     @classmethod
@@ -42,9 +43,7 @@ class MySeleniumTests(LiveServerTestCase):
         self.selenium.find_element("name", "password1").send_keys('6:}jga,6mRKNUqMQ')
         self.selenium.find_element("name", "password2").send_keys('6:}jga,6mRKNUqMQ')
         self.selenium.find_element("name", "signup_button").click()
-        WebDriverWait(self.selenium, 30).until(
-            EC.presence_of_element_located((By.TAG_NAME, "body"))
-        )
+        WebDriverWait(self.selenium, 30).until(EC.presence_of_element_located((By.TAG_NAME, "body")))
         body = self.selenium.find_element('tag name', 'body')
         self.assertIn('bugbugbug (0 Pts)', body.text)
 
@@ -54,9 +53,7 @@ class MySeleniumTests(LiveServerTestCase):
         self.selenium.find_element("name", "login").send_keys('bugbug')
         self.selenium.find_element("name", "password").send_keys('secret')
         self.selenium.find_element("name", "login_button").click()
-        WebDriverWait(self.selenium, 30).until(
-            EC.presence_of_element_located((By.TAG_NAME, "body"))
-        )
+        WebDriverWait(self.selenium, 30).until(EC.presence_of_element_located((By.TAG_NAME, "body")))
         body = self.selenium.find_element('tag name', 'body')
         self.assertIn('bugbug (0 Pts)', body.text)
 
@@ -76,11 +73,36 @@ class MySeleniumTests(LiveServerTestCase):
         Imagepath = os.path.abspath(os.path.join(os.getcwd(), 'website/static/img/background.jpg'))
         self.selenium.find_element("name", "screenshots").send_keys(Imagepath)
         # pass captacha if in test mode
-        self.selenium.find_element("name","captcha_1").send_keys('PASSED')
+        self.selenium.find_element("name", "captcha_1").send_keys('PASSED')
         self.selenium.find_element("name", "reportbug_button").click()
         self.selenium.get('%s%s' % (self.live_server_url, '/all_activity/'))
-        WebDriverWait(self.selenium, 30).until(
-            EC.presence_of_element_located((By.TAG_NAME, "body"))
-        )
+        WebDriverWait(self.selenium, 30).until(EC.presence_of_element_located((By.TAG_NAME, "body")))
         body = self.selenium.find_element('tag name', 'body')
         self.assertIn('Description of bug', body.text)
+
+
+class HideImage(TestCase):
+    def setUp(self):
+        test_issue = Issue.objects.create(description="test", url="test.com")
+        test_issue.screenshot = SimpleUploadedFile(name='test_image.jpg', content=open(f"website/static/images/dummy-user.png", 'rb').read(), content_type='image/png')
+        test_issue.save()
+
+    def test_on_hide(self):
+
+        Test_Object = Issue.objects.get(url="test.com")
+        issue_screenshot_list_orignal = IssueScreenshot.objects.filter(issue=Test_Object.id)
+
+        Test_Object.is_hidden = True
+        Test_Object.save()
+        issue_screenshot_list_new = IssueScreenshot.objects.filter(issue=Test_Object.id)
+
+        for screenshot in issue_screenshot_list_orignal:
+            filename = screenshot.image.name
+
+            if default_storage.exists(filename):
+                self.assertTrue(False, "files exist")
+        for screenshot in issue_screenshot_list_new:
+            filename = screenshot.image.name
+
+            if "hidden" not in filename:
+                self.assertFalse(True, "files rename failed")
