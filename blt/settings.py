b337c048392a25eec1a941f09cf8d26e75b59cb7
AtmegaBuzz <swapnilshinde9382@gmail.com>
2023-05-16 21:27:16 +0530

    support for API rate throttling

--- blt/settings.py
+++ blt/settings.py
@@ -486,3 +486,20 @@ if "test" in sys.argv:
 RECAPTCHA_PUBLIC_KEY =  os.environ.get("RECAPTCHA_PUBLIC_KEY", "test")
 RECAPTCHA_PRIVATE_KEY = os.environ.get("RECAPTCHA_PRIVATE_KEY", "test")
 RECAPTCHA_REQUIRED_SCORE = 0.85
+
+
+
+REST_FRAMEWORK = {
+
+    'DEFAULT_AUTHENTICATION_CLASSES':[
+        'rest_framework.authentication.TokenAuthentication'
+    ],
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '100/day',
+        'user': '1000/day'
+    }
+}
