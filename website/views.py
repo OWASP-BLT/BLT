b93d03b233b07d828a42b5f3b5257b18bd35fcf7
JisanAR03 <altafurrahaman3@gmail.com>
2023-10-18 17:44:32 +0600

    fixed some mistakes in views.py

--- website/views.py
+++ website/views.py
@@ -2033,20 +2033,20 @@ def comment_on_issue(request, issue_pk):
     return render(request, "comments2.html",context)
 
 def delete_comment(request):
-    issue = Issue.objects.get(pk=request.POST['issue_pk'])
+    int_issue_pk = int(request.POST['issue_pk'])
+    issue = Issue.objects.get(pk=int_issue_pk)
     if request.method == "POST":
         all_comment = Comment.objects.filter(issue=issue)
-        print(request.POST['issue_pk'])
         comment = Comment.objects.get(pk=int(request.POST['comment_pk']))
+        if request.user.username != comment.author:
+            return HttpResponse("You are not authorized to delete this comment")
         try:
             show = comment.parent.pk
         except:
             show = -1
-        if request.user.username != comment.author:
-            return HttpResponse("Cannot delete this comment")
         comment.delete()
     context = {
-        "all_comment": Comment.objects.filter(issue__id=int(request.POST['issue_pk'])).order_by("-created_date"),
+        "all_comment": Comment.objects.filter(issue__id=int_issue_pk).order_by("-created_date"),
         "object": issue,
     }
     return render(request, "comments2.html", context)
