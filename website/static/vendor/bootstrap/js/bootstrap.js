6ce5801ff4f5aa5c7b12a6270a2ef83f3555c7d2
JisanAR03 <altafurrahaman3@gmail.com>
2023-11-01 18:30:57 +0600

    Fix code scanning alert issue-#1357

--- website/static/vendor/bootstrap/js/bootstrap.js
+++ website/static/vendor/bootstrap/js/bootstrap.js
@@ -713,8 +713,8 @@ function sanitizeInput(input) {
 
     function getTargetFromTrigger($trigger) {
         var href
-        var target = $trigger.attr('data-target')
-            || (href = $trigger.attr('href')) && href.replace(/.*(?=#[^\s]+$)/, '') // strip for ie7
+        var target = sanitizeSelector($trigger.attr('data-target'))
+            || (href = sanitizeSelector($trigger.attr('href'))) && href.replace(/.*(?=#[^\s]+$)/, '') // strip for ie7
 
         return $(target)
     }
