2c7addc7a2e0851f0660129203ddc7b68ff6dd43
JisanAR03 <altafurrahaman3@gmail.com>
2023-11-02 16:05:23 +0600

    fixed security issue #1350 & #1349

--- website/static/js/jquery.caret.js
+++ website/static/js/jquery.caret.js
@@ -216,8 +216,19 @@
             $inputor = this.$inputor;
             if (oDocument.selection) {
                 offset = this.getIEOffset(pos);
-                offset.top += $(oWindow).scrollTop() + $inputor.scrollTop();
-                offset.left += $(oWindow).scrollLeft() + $inputor.scrollLeft();
+                // Check if oWindow is a window object by checking for window-specific properties
+                function isValidWindow(obj) {
+                    return obj && typeof obj === 'object' && 'scrollTo' in obj && obj.document && obj.self === obj;
+                }
+                // Then use this function in your conditionals
+                if (isValidWindow(oWindow)) {
+                    offset.top += $(oWindow).scrollTop();
+                    offset.left += $(oWindow).scrollLeft();
+                } else {
+                    console.error('oWindow is not a valid window object.');
+                }
+                offset.top += $inputor.scrollTop();
+                offset.left += $inputor.scrollLeft();
                 return offset;
             } else {
                 offset = $inputor.offset();
