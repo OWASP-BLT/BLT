e2f94f87de066103473069ea19b02d1cec9ffe6e
JisanAR03 <altafurrahaman3@gmail.com>
2023-11-05 00:10:08 +0600

    fix security issue #1329

--- website/views.py
+++ website/views.py
@@ -735,7 +735,7 @@ class IssueCreate(IssueBaseCreate, CreateView):
                 return JsonResponse("Created", safe=False)
             else:
                 self.process_issue(self.request.user, obj, domain_exists, domain)
-                return HttpResponseRedirect(redirect_url)
+                return HttpResponseRedirect(redirect_url) 
         
         return create_issue(self,form)
 
