d0e56c9501da905e152f3f9480ebbecaf30bc1f1
DonnieBLT <128622481+DonnieBLT@users.noreply.github.com>
2024-02-15 19:30:27 -0500

    Update settings.py

--- blt/settings.py
+++ blt/settings.py
@@ -318,7 +318,7 @@ ACCOUNT_EMAIL_VERIFICATION = "optional"
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
 # Allow all host headers
-ALLOWED_HOSTS = ["." + DOMAIN_NAME, "127.0.0.1", "localhost", "0.0.0.0", "blt.owasp.org", DOMAIN_NAME_PREVIOUS]
+ALLOWED_HOSTS = ["." + DOMAIN_NAME, "127.0.0.1", "localhost", "0.0.0.0", "blt.owasp.org", "." + DOMAIN_NAME_PREVIOUS]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
