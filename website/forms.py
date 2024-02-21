a0f2bf3a9d46a5244aefc208f0d872a2b63eb352
Arkadii Yakovets <arkadii.yakovets@owasp.org>
2024-02-20 17:08:18 -0800

    Add pre-commit config
    Include changes automatically generated by isort and ruff.

--- website/forms.py
+++ website/forms.py
@@ -1,28 +1,40 @@
-from django import forms
-
-from .models import InviteFriend, UserProfile, Hunt
-from mdeditor.fields import MDTextFormField
 from captcha.fields import CaptchaField
+from django import forms
+from mdeditor.fields import MDTextFormField
+
+from .models import InviteFriend, UserProfile
+
 
 class FormInviteFriend(forms.ModelForm):
     class Meta:
         model = InviteFriend
-        fields = ['recipient']
-        widgets = {'recipient': forms.TextInput(attrs={'class': 'form-control'})}
+        fields = ["recipient"]
+        widgets = {"recipient": forms.TextInput(attrs={"class": "form-control"})}
 
 
 class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
-        fields = ('user_avatar',)
+        fields = ("user_avatar",)
 
-class HuntForm (forms.Form): 
-    content = MDTextFormField ()
-    start_date = forms.DateTimeField(widget=forms.DateTimeInput(attrs={'class': 'col-sm-6', 'readonly' : True}),label='', required=False ) 
-    end_date = forms.DateTimeField(widget=forms.DateTimeInput(attrs={'class': 'col-sm-6', 'readonly' : True}),label='', required=False)
+
+class HuntForm(forms.Form):
+    content = MDTextFormField()
+    start_date = forms.DateTimeField(
+        widget=forms.DateTimeInput(attrs={"class": "col-sm-6", "readonly": True}),
+        label="",
+        required=False,
+    )
+    end_date = forms.DateTimeField(
+        widget=forms.DateTimeInput(attrs={"class": "col-sm-6", "readonly": True}),
+        label="",
+        required=False,
+    )
+
 
 class CaptchaForm(forms.Form):
     captcha = CaptchaField()
+
 
 class QuickIssueForm(forms.Form):
     url = forms.CharField()
