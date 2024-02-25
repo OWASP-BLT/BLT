d1b5b02315f702cdf3e0312627ee6d37b1aec1d4
JisanAR03 <altafurrahaman3@gmail.com>
2024-02-25 21:58:03 +0600

    fix invite

--- website/migrations/0083_alter_invitefriend_options_and_more.py
+++ website/migrations/0083_alter_invitefriend_options_and_more.py
@@ -7,7 +7,6 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ("website", "0082_issue_reporter_ip_address"),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
