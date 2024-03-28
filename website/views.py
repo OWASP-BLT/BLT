db6a3f2c8881cddf2ccd7566bd983699e45ed17e
HanilJain <shirshjain0111@gmail.com>
2024-03-28 23:40:09 +0530

    commit

--- website/views.py
+++ website/views.py
@@ -1439,7 +1439,7 @@ class DomainDetailView(ListView):
             .annotate(c=Count("label"))
             .order_by()
         )
-        domain_name = Domain.get_name
+        domain_name = domain.get_name
         context["twitter_url"] = "https://twitter.com/%s" + (domain.get_or_set_x_url(domain_name))
 
         return context
