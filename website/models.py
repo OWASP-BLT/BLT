ba4ad63354365df41f324df1a2d7117ec69d6fb5
Uttkarsh-raj <uttkarsh.raj.jsr@gmail.com>
2024-05-10 19:59:31 +0530

    company_id not setting while creating company

--- website/models.py
+++ website/models.py
@@ -62,6 +62,11 @@ class Company(models.Model):
 
     def __str__(self):
         return self.name
+    
+    def save(self, *args, **kwargs):
+        if not self.company_id:
+            self.company_id = str(uuid.uuid4())
+        super().save(*args, **kwargs)
 
 
 class Domain(models.Model):
