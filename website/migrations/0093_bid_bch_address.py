bcf197e9444aa1ba1121f6d409946503fab5442a
Sarthak5598 <sarthak5598sharma@gmail.com>
2024-05-14 12:04:00 +0530

    linting

--- website/migrations/0093_bid_bch_address.py
+++ website/migrations/0093_bid_bch_address.py
@@ -4,7 +4,6 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ("website", "0092_merge_0091_bid_0091_remove_company_company_id"),
     ]
