22fa12bbc2a82303d83ee40bf1ce869f2996d486
H4N1L <119354421+HanilJain@users.noreply.github.com>
2024-07-16 07:52:24 +0530

    Update website/models.py
    Co-authored-by: Arkadii Yakovets <2201626+arkid15r@users.noreply.github.com>

--- website/models.py
+++ website/models.py
@@ -277,8 +277,8 @@ class Issue(models.Model):
     cve_score = models.DecimalField(max_digits=2, decimal_places=1, null=True, blank=True)
     infringing_domain_name = models.TextField(max_length=100, null=True, blank=True)
     infringing_domain_url = models.URLField(default="", null=True, blank=True)
-    registration_no = models.PositiveIntegerField(null=True, blank=True)
-    serial_no = models.PositiveIntegerField(null=True, blank=True)
+    registration_number = models.PositiveIntegerField(null=True, blank=True)
+    serial_number = models.PositiveIntegerField(null=True, blank=True)
     contact_info = models.CharField(max_length=100, null=True, blank=True)
 
     def __unicode__(self):
