af389c764275852fdea6dd7e84921208e6820669
Sarthak5598 <sarthak5598sharma@gmail.com>
2024-07-26 14:07:21 +0530

    added caching

--- blt/middleware/ip_restrict.py
+++ blt/middleware/ip_restrict.py
@@ -1,3 +1,4 @@
+from django.core.cache import cache
 from django.http import HttpResponseForbidden
 
 from website.models import BlockedIP
@@ -11,16 +12,20 @@ class IPRestrictMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
-    def blocked_ips(self, request):
-        # Retrieve all blocked IP addresses from the database
-        blocked_addresses = BlockedIP.objects.values_list("address", flat=True)
-        return set(blocked_addresses)  # Use a set for faster lookup
+    def blocked_ips(self):
+        blocked_ips = cache.get("blocked_ips")
+        if blocked_ips is None:
+            # Retrieve all blocked IP addresses from the database
+            blocked_addresses = BlockedIP.objects.values_list("address", flat=True)
+            blocked_ips = set(blocked_addresses)
+            cache.set("blocked_ips", blocked_ips, timeout=3600)
+        return blocked_ips
 
     def __call__(self, request):
         ip = request.META.get("REMOTE_ADDR")
 
         if ip:
-            blocked_ips = self.blocked_ips(request)
+            blocked_ips = self.blocked_ips()
             if ip in blocked_ips:
                 return HttpResponseForbidden(
                     "Your IP address is restricted from accessing this site."
