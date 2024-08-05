906d1c5d7abdb8d1a1df32162b99a204ad426f8c
DonnieBLT <128622481+DonnieBLT@users.noreply.github.com>
2024-08-04 22:14:13 -0400

    refactor: Update langchain imports and remove unused code in bot.py

--- website/bot.py
+++ website/bot.py
@@ -4,32 +4,32 @@ import openai
 from django.core.files.storage import default_storage
 from dotenv import find_dotenv, load_dotenv
 from langchain.chains import ConversationalRetrievalChain
-from langchain.chat_models import ChatOpenAI
-from langchain.embeddings import OpenAIEmbeddings
 from langchain.memory import ConversationSummaryMemory
-from langchain.prompts import ChatPromptTemplate
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import (
-    DirectoryLoader,
     Docx2txtLoader,
     PyPDFLoader,
     TextLoader,
     UnstructuredMarkdownLoader,
 )
 from langchain_community.vectorstores import FAISS
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from openai import OpenAI
 
 load_dotenv(find_dotenv(), override=True)
 
 
 def is_api_key_valid(api_key):
-    openai.api_key = api_key
+    client = OpenAI(api_key=api_key)
     try:
-        openai.Completion.create(prompt="Hello", model="gpt-3.5-turbo-instruct", max_tokens=1)
+        client.completions.create(prompt="Hello", model="gpt-3.5-turbo-instruct", max_tokens=1)
         return True
-    except openai.error.APIConnectionError as e:
+    except openai.APIConnectionError as e:
         print(f"Failed to connect to OpenAI API: {e}")
-    except openai.error.RateLimitError as e:
+    except openai.RateLimitError as e:
         print(f"OpenAI API rate limit exceeded: {e}")
-    except openai.error.OpenAIError as e:
+    except openai.APIError as e:
         print(f"OpenAI API error: {e}")
     return False
 
@@ -42,31 +42,44 @@ def load_document(file_path):
         ".md": UnstructuredMarkdownLoader,
     }
 
-    file_path = Path(file_path)
-    extension = file_path.suffix
+    extension = Path(file_path).suffix
     Loader = loaders.get(extension)
 
     if Loader is None:
         raise ValueError(f"Unsupported file format: {extension}")
 
-    return Loader(str(file_path)).load()  # Convert Path to string
+    with default_storage.open(file_path) as f:
+        return Loader(f).load()
 
 
 def load_directory(dir_path):
-    dir_path = str(Path(dir_path))  # Convert Path to string
-    return DirectoryLoader(dir_path).load()
+    files = default_storage.listdir(dir_path)[1]
+    documents = []
+    for file_name in files:
+        file_path = Path(dir_path) / file_name
+        documents.extend(load_document(file_path))
+    return documents
+
+
+def split_document(chunk_size, chunk_overlap, document):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=chunk_size,
+        chunk_overlap=chunk_overlap,
+        length_function=len,
+    )
+    return text_splitter.split_documents(document)
 
 
 def embed_documents_and_save(embed_docs):
-    db_folder_path = "faiss_index"  # Use a relative path
+    db_folder_path = "faiss_index"
 
-    embeddings = OpenAIEmbeddings(model="text-embedding-ada-002")
+    embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
 
     # Check if the folder exists in the storage system
     if default_storage.exists(db_folder_path) and default_storage.listdir(db_folder_path):
         # Load the FAISS index directly from the storage
         db_files = [
-            default_storage.path(f"{db_folder_path}/{file_name}")
+            default_storage.open(db_folder_path + "/" + file_name)
             for file_name in default_storage.listdir(db_folder_path)[1]
         ]
         db = FAISS.load_local(db_files, embeddings, allow_dangerous_deserialization=True)
@@ -83,12 +96,12 @@ def embed_documents_and_save(embed_docs):
 
 
 def load_vector_store():
-    embeddings = OpenAIEmbeddings(model="text-embedding-ada-002")
-    db_folder_path = "faiss_index"  # Use a relative path
+    embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
+    db_folder_path = "faiss_index"
 
     # Load the FAISS index directly from the storage
     db_files = [
-        default_storage.path(f"{db_folder_path}/{file_name}")
+        default_storage.open(db_folder_path + "/" + file_name)
         for file_name in default_storage.listdir(db_folder_path)[1]
     ]
     db = FAISS.load_local(db_files, embeddings, allow_dangerous_deserialization=True)
@@ -100,22 +113,20 @@ def conversation_chain(vector_store):
     retrieval_search_results = 5
     summary_max_memory_token_limit = 1500
     prompt = ChatPromptTemplate.from_messages(
-        [
+        (
+            "human",
             (
-                "human",
-                (
-                    "You are an assistant specifically designed for answering questions about "
-                    "the OWASP Bug Logging Tool (BLT) application. Use the following pieces of "
-                    "retrieved context to answer the question. If the user's question is not "
-                    "related to the BLT application or if the context does not provide enough "
-                    "information to answer the question, respond with 'Please ask a query related "
-                    "to the BLT Application.' Ensure your response is concise and does not exceed "
-                    "three sentences.\nQuestion: {question}\nContext: {context}\nAnswer:"
-                ),
+                "You are an assistant specifically designed for answering questions about "
+                "the OWASP Bug Logging Tool (BLT) application. Use the following pieces of "
+                "retrieved context to answer the question. If the user's question is not "
+                "related to the BLT application or if the context does not provide enough "
+                "information to answer the question, respond with 'Please ask a query related "
+                "to the BLT Application.' Ensure your response is concise and does not exceed "
+                "three sentences.\nQuestion: {question}\nContext: {context}\nAnswer:"
             ),
-        ]
+        )
     )
-    llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0.5)
+    llm = ChatOpenAI(model_name="gpt-3.5-turbo-0125", temperature=0.5)
     retriever = vector_store.as_retriever(
         search_type="similarity", search_kwargs={"k": retrieval_search_results}
     )
