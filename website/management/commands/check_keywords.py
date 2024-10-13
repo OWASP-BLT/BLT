64fa0bb6529c640288911878fa8a61ac364572ac
sagnik3788 <116512372+sagnik3788@users.noreply.github.com>
2024-10-14 01:43:30 +0530

    precommit

--- website/management/commands/check_keywords.py
+++ website/management/commands/check_keywords.py
@@ -1,12 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
 from django.core.management.base import BaseCommand
-from website.models import Monitor 
 from django.utils import timezone
+
+from website.models import Monitor
 
 
 class Command(BaseCommand):
-    help = 'Checks for keywords in monitored URLs'
+    help = "Checks for keywords in monitored URLs"
 
     def handle(self, *args, **options):
         monitors = Monitor.objects.all()
@@ -15,14 +16,13 @@ class Command(BaseCommand):
                 response = requests.get(monitor.url)
                 response.raise_for_status()
 
-                soup = BeautifulSoup(response.content, 'html.parser')
+                soup = BeautifulSoup(response.content, "html.parser")
                 page_content = soup.get_text()
 
                 if monitor.keyword in page_content:
-                    new_status = 'UP'
+                    new_status = "UP"
                 else:
-                    new_status = 'DOWN'
-
+                    new_status = "DOWN"
 
                 if monitor.status != new_status:
                     monitor.status = new_status
@@ -30,8 +30,8 @@ class Command(BaseCommand):
                 monitor.last_checked_time = timezone.now()
                 monitor.save()
 
-                self.stdout.write(self.style.SUCCESS(
-                    f'Monitoring {monitor.url}: status {monitor.status}'))
+                self.stdout.write(
+                    self.style.SUCCESS(f"Monitoring {monitor.url}: status {monitor.status}")
+                )
             except Exception as e:
-                self.stderr.write(self.style.ERROR(
-                    f'Error monitoring {monitor.url}: {str(e)}'))
+                self.stderr.write(self.style.ERROR(f"Error monitoring {monitor.url}: {str(e)}"))
