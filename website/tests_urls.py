c85e0a1d95ab5d09ca578c863fff8472473c4c48
JisanAR <altafurrahaman3@gmail.com>
2024-10-23 10:04:40 +0600

    fix pre commit issue

--- website/tests_urls.py
+++ website/tests_urls.py
@@ -2,7 +2,9 @@ import importlib
 import os
 
 import chromedriver_autoinstaller
+from allauth.socialaccount.models import SocialApp
 from django.conf import settings
+from django.contrib.sites.models import Site
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from django.db import transaction
 from django.urls import reverse
@@ -31,6 +33,21 @@ class UrlsTest(StaticLiveServerTestCase):
     def tearDownClass(cls):
         cls.selenium.quit()
         super(UrlsTest, cls).tearDownClass()
+
+    def setUp(self):
+        site = Site.objects.get(pk=1)
+        site.domain = "localhost:8082"
+        site.name = "localhost"
+        site.save()
+
+        # Create SocialApp for GitHub
+        github_app = SocialApp.objects.create(
+            provider="github",
+            name="GitHub",
+            client_id="dummy_client_id",
+            secret="dummy_secret",
+        )
+        github_app.sites.add(site)
 
     def test_responses(
         self,
