a9ec665386c1ced966a5db39c21482ffced98a56
DonnieBLT <128622481+DonnieBLT@users.noreply.github.com>
2024-10-26 10:10:17 -0400

    Revert "Move .env.examples to project root and include setup for DATABASE_URL…"
    This reverts commit af861f8f9222619a2967446f1f15b9dfeb1bd529.

--- blt/settings.py
+++ blt/settings.py
@@ -16,8 +16,8 @@ import environ
 from django.utils.translation import gettext_lazy as _
 
 env = environ.Env()
-# reading .env file from parent directory
-environ.Env.read_env(os.path.join(os.path.dirname(os.path.dirname(__file__)), ".env"))
+# reading .env file
+environ.Env.read_env()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
