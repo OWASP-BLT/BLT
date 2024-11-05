85416b2a466ded83fbe9ca6dd9e9b1c143d577ff
Sarthak5598 <sarthak5598sharma@gmail.com>
2024-11-05 21:30:39 +0530

    updated url pattern

--- blog/urls.py
+++ blog/urls.py
@@ -6,8 +6,8 @@ from . import views
 
 urlpatterns = [
     path("", views.PostListView.as_view(), name="post_list"),
-    path("post/new/", views.PostCreateView.as_view(), name="post_create"),
-    path("post/<slug:slug>/", views.PostDetailView.as_view(), name="post_detail"),
-    path("post/<slug:slug>/edit/", views.PostUpdateView.as_view(), name="post_update"),
-    path("post/<slug:slug>/delete/", views.PostDeleteView.as_view(), name="post_delete"),
+    path("new/", views.PostCreateView.as_view(), name="post_create"),
+    path("<slug:slug>/", views.PostDetailView.as_view(), name="post_detail"),
+    path("<slug:slug>/edit/", views.PostUpdateView.as_view(), name="post_update"),
+    path("<slug:slug>/delete/", views.PostDeleteView.as_view(), name="post_delete"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
