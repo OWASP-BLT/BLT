de2b30a1a1d8f597a0367edd740c7102f8aa6f74
DonnieBLT <128622481+DonnieBLT@users.noreply.github.com>
2024-11-24 17:49:59 -0500

    Refactor file upload verification to include default storage import in Post model

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
-from django.core.files.storage import default_storage
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
@@ -26,6 +25,7 @@ class Post(models.Model):
 @receiver(post_save, sender=Post)
 def verify_file_upload(sender, instance, **kwargs):
     from django.conf import settings
+    from django.core.files.storage import default_storage
 
     print("Verifying file upload...")
     print(f"DEFAULT_FILE_STORAGE setting: {settings.DEFAULT_FILE_STORAGE}")
