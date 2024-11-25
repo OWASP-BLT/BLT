b96c9184652f95eb792b51e9bf799a59d27b455c
Dhruv Trivedi <95343534+drvcodenta@users.noreply.github.com>
2024-11-25 13:05:02 +0530

    retain secure_proxy_ssl_header and pass run test

--- blt/settings.py
+++ blt/settings.py
@@ -310,7 +310,7 @@ ACCOUNT_EMAIL_VERIFICATION = "optional"
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 
-# SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
 # Allow all host headers
 ALLOWED_HOSTS = [
