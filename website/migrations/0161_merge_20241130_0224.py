1e01f6770ecab1f71e3d1089bbc2a5f7a1ac6451
Krrish Sehgal <133865424+krrish-sehgal@users.noreply.github.com>
2024-11-30 07:56:23 +0530

    fix migratoins

--- website/migrations/0161_merge_20241130_0224.py
+++ website/migrations/0161_merge_20241130_0224.py
@@ -4,7 +4,6 @@ from django.db import migrations
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ("website", "0160_activity_dislike_count_activity_dislikes_and_more"),
         ("website", "0160_merge_20241129_0712"),
