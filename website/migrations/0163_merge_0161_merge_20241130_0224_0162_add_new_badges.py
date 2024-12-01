d52f5efe60c81d39a521f796b46cd34c8e6d89f1
Krrish Sehgal <133865424+krrish-sehgal@users.noreply.github.com>
2024-12-01 22:32:06 +0530

    pre fix

--- website/migrations/0163_merge_0161_merge_20241130_0224_0162_add_new_badges.py
+++ website/migrations/0163_merge_0161_merge_20241130_0224_0162_add_new_badges.py
@@ -4,7 +4,6 @@ from django.db import migrations
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ("website", "0161_merge_20241130_0224"),
         ("website", "0162_add_new_badges"),
