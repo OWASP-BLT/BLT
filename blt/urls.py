99ca82fef897ff1cf0232f68c57977b3aded7a3b
Krrish Sehgal <133865424+krrish-sehgal@users.noreply.github.com>
2024-12-03 09:33:18 +0530

    fix

--- blt/urls.py
+++ blt/urls.py
@@ -164,6 +164,7 @@ from website.views.user import (
     deletions,
     follow_user,
     get_score,
+    github_webhook,
     invite_friend,
     profile,
     profile_edit,
@@ -173,7 +174,6 @@ from website.views.user import (
     user_dashboard,
     users_view,
     withdraw,
-    github_webhook,
 )
 
 favicon_view = RedirectView.as_view(url="/static/favicon.ico", permanent=True)
@@ -639,7 +639,7 @@ urlpatterns = [
     ),
     path("delete_time_entry/", delete_time_entry, name="delete_time_entry"),
     path("assign-badge/<str:username>/", assign_badge, name="assign_badge"),
-    path('github-webhook/', github_webhook, name='github-webhook'),
+    path("github-webhook/", github_webhook, name="github-webhook"),
 ]
 
 if settings.DEBUG:
