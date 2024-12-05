bee258fc19aa80c2447d0a2aeb3e5740b047e892
SahilDhillon21 <sodhillon_b21@it.vjti.ac.in>
2024-12-05 17:45:44 +0530

    sp

--- website/migrations/0157_badge_activity_dislike_count_activity_dislikes_and_more.py
+++ website/migrations/0157_badge_activity_dislike_count_activity_dislikes_and_more.py
@@ -7,7 +7,6 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ("website", "0156_merge_20241205_1211"),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
