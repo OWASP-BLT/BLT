d337babd7331b8d7e609e3b3720ef63f6be52369
Krrish Sehgal <133865424+krrish-sehgal@users.noreply.github.com>
2024-12-19 00:57:28 +0530

    fixes

--- website/migrations/0173_challenge.py
+++ website/migrations/0173_challenge.py
@@ -5,7 +5,6 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ("website", "0172_merge_20241218_0505"),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
