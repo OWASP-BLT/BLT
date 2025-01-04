b702b7375f0e6f3df4b6c5ce30965f727f93b033
Krrish Sehgal <133865424+krrish-sehgal@users.noreply.github.com>
2025-01-05 01:45:40 +0530

    chages

--- website/views/issue.py
+++ website/views/issue.py
@@ -1501,6 +1501,7 @@ def comment_on_content(request, content_pk):
     content_type = request.POST.get("content_type")
     content_type_obj = ContentType.objects.get(model=content_type)
     content = content_type_obj.get_object_for_this_type(pk=content_pk)
+    VALID_CONTENT_TYPES = ["issue", "post"]
 
     if request.method == "POST" and isinstance(request.user, User):
         comment = escape(request.POST.get("comment", ""))
@@ -1514,6 +1515,10 @@ def comment_on_content(request, content_pk):
             replying_to_comment_id = replying_to_input[1]
 
             parent_comment = Comment.objects.filter(pk=replying_to_comment_id).first()
+
+            if content_type not in VALID_CONTENT_TYPES:
+                messages.error(request, "Invalid content type.")
+                return redirect("home")
 
             if parent_comment is None:
                 messages.error(request, "Parent comment doesn't exist.")
