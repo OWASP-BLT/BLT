b51339cf64375d90cf5a6677401609b6a4908335
Krishna Kaushal <104532938+tsu-ki@users.noreply.github.com>
2025-01-19 21:26:25 +0530

    Creating slack-bot for OWASP slack workspace (#3145)
    * slackbot initial commit
    
    * slack bot integration
    
    * major changes
    
    * resolving CI/CD
    
    * CI/CD
    
    * fixed pre-commit
    
    * fixing

--- blt/urls.py
+++ blt/urls.py
@@ -187,6 +187,7 @@ from website.views.project import (
     select_contribution,
 )
 from website.views.slack_handlers import slack_events
+from website.views.slackbot import slack_commands
 from website.views.teams import (
     TeamOverview,
     add_member,
@@ -289,7 +290,8 @@ urlpatterns = [
     re_path(r"^auth/github/connect/$", GithubConnect.as_view(), name="github_connect"),
     re_path(r"^auth/google/connect/$", GoogleConnect.as_view(), name="google_connect"),
     path("auth/github/url/", github_views.oauth2_login),
-    path("oauth/slack/callback/", SlackCallbackView.as_view(), name="slack_callback"),
+    path("oauth/slack/callback/", SlackCallbackView.as_view(), name="slack_oauth_callback"),
+    path("slack/commands/", slack_commands, name="slack_commands"),
     path("auth/google/url/", google_views.oauth2_login),
     path("auth/facebook/url/", facebook_views.oauth2_callback),
     path("socialaccounts/", SocialAccountListView.as_view(), name="social_account_list"),
