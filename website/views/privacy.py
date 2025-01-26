75b6e63c58c01ded40b27fa91b5eba142b858fd2
Krishna Kaushal <sendittokrishnaa@gmail.com>
2025-01-26 17:01:43 +0530

    again, fixing pre-commit

--- website/views/privacy.py
+++ website/views/privacy.py
@@ -16,7 +16,6 @@ def overlay_faces(img, color=(255, 255, 255)):
             img = cv2.cvtColor(img, cv2.COLOR_BGRA2BGR)
 
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
         face_cascade = cv2.CascadeClassifier(
             cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
         )
