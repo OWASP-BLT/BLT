22bc01850cda105fbab0cd17cc5c8b62f108927d
Krrish Sehgal <133865424+krrish-sehgal@users.noreply.github.com>
2025-01-27 01:23:03 +0530

    pre fix

--- website/migrations/0186_merge_0185_merge_20250126_1451_0185_room.py
+++ website/migrations/0186_merge_0185_merge_20250126_1451_0185_room.py
@@ -4,7 +4,6 @@ from django.db import migrations
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ("website", "0185_merge_20250126_1451"),
         ("website", "0185_room"),
