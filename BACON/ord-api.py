921137b1fd7f5b8cbf4b00053fe31229ea52cbb3
Krrish Sehgal <133865424+krrish-sehgal@users.noreply.github.com>
2025-02-20 00:39:52 +0530

    pre-commit fixes

--- BACON/ord-api.py
+++ BACON/ord-api.py
@@ -1,35 +1,38 @@
-from flask import Flask, request, jsonify
 import subprocess
-import os
+
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 ORD_PATH = "/usr/local/bin/ord"
 YAML_FILE_PATH = "/blockchain/ord-flask-server/tmp-split.yaml"
 
+
 @app.route("/send-bacon-tokens", methods=["POST"])
 def send_bacon_tokens():
     # Extract YAML content from request
     yaml_content = request.json.get("yaml_content")
-    
+
     if not yaml_content:
         return jsonify({"success": False, "error": "YAML content missing"}), 400
 
     # Write YAML to a temporary file
-    with open(YAML_FILE_PATH, 'w') as file:
+    with open(YAML_FILE_PATH, "w") as file:
         file.write(yaml_content)
-    #change the ip
+    # change the ip
     command = [
-        "sudo", ORD_PATH,
+        "sudo",
+        ORD_PATH,
         "--bitcoin-rpc-username=bitcoin",
         "--bitcoin-rpc-password=bitcoin",
         "--bitcoin-rpc-url=http://bitcoin-node-ip:18443",
-        "-r", "wallet",
+        "-r",
+        "wallet",
         "--server-url=http://ord-server-ip:9001",
         "--name=ord",
         "split",
         f"--splits={YAML_FILE_PATH}",
-        "--fee-rate=1"
+        "--fee-rate=1",
     ]
 
     try:
@@ -38,6 +41,6 @@ def send_bacon_tokens():
     except subprocess.CalledProcessError as e:
         return jsonify({"success": False, "error": e.stderr})
 
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=9002)
-
