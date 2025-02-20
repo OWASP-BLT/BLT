793977ab3a026d512431c9cc20ffe28dca8cf80d
Nikhil Patil <nikhilpatil222003@gmail.com>
2025-02-21 01:34:31 +0530

    Apply ruff-format fixes

--- website/migrations/0204_remove_organization_unique_organization_slug_and_more.py
+++ website/migrations/0204_remove_organization_unique_organization_slug_and_more.py
@@ -4,7 +4,6 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ("website", "0203_remove_organization_unique_organization_slug_and_more"),
     ]
