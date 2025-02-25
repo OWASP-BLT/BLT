44d059c111f0989699b34f3d340b495f2e1bff2c
iitzIrFan <irfanulhawk@gmail.com>
2025-02-25 21:30:30 +0530

    Fixes

--- website/test_badge_views.py
+++ website/test_badge_views.py
@@ -65,8 +65,8 @@ class BadgeViewsTest(TestCase):
 
     def test_repo_badge_view(self):
         """Test that repo badge view returns correct chart with visit count"""
-        repo = get_object_or_404(Repo, slug=slug)
-        return JsonResponse({"repo": repo.name, "visit_count": repo.visit_count})
+        url = reverse("repo-badge", kwargs={"slug": self.repo.slug})
+        response = self.client.get(url)
 
         # Check response basics
         self.assertEqual(response.status_code, 200)
