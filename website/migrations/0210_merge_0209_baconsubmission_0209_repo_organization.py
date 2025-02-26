cabdabd1cffe8b14c17ea8d567a6aba31b952aa8
Krrish Sehgal <133865424+krrish-sehgal@users.noreply.github.com>
2025-02-27 01:24:19 +0530

    pre commit fix

--- website/migrations/0210_merge_0209_baconsubmission_0209_repo_organization.py
+++ website/migrations/0210_merge_0209_baconsubmission_0209_repo_organization.py
@@ -4,7 +4,6 @@ from django.db import migrations
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ("website", "0209_baconsubmission"),
         ("website", "0209_repo_organization"),
