cf08b2c864659c3342067e4db12e0eb73b8ecaa2
gojo-satorou-v7 <gojoxitadori1@gmail.com>
2025-03-02 00:29:47 +0530

    fix ssrf vulnerability by removing the unnecessary/unused fetching logic of favicon.ico

--- website/views/user.py
+++ website/views/user.py
@@ -175,17 +175,7 @@ class InviteCreate(TemplateView):
         domain = None
         if email:
             domain = email.split("@")[-1]
-            try:
-                full_url_domain = "https://" + domain + "/favicon.ico"
-                if is_valid_https_url(full_url_domain):
-                    safe_url = rebuild_safe_url(full_url_domain)
-                    response = requests.get(safe_url, timeout=5)
-                    if response.status_code == 200:
-                        exists = "exists"
-            except:
-                pass
         context = {
-            "exists": exists,
             "domain": domain,
             "email": email,
         }
