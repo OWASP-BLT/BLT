f703cf80399d89da4d745a27921067d304dab7f4
igennova <luckynegi1025@gmail.com>
2025-03-02 21:10:56 +0530

    let's hope

--- website/migrations/0213_organization_categories_and_more.py
+++ website/migrations/0213_organization_categories_and_more.py
@@ -6,7 +6,6 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ("website", "0212_course_lecture_lecturestatus_rating_section_and_more"),
     ]
