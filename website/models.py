271ebd84d236d9c11e52404de4469fa8bf4eb40b
igennova <luckynegi1025@gmail.com>
2025-03-02 20:04:17 +0530

    done

--- website/models.py
+++ website/models.py
@@ -125,7 +125,7 @@ class Organization(models.Model):
     logo = models.ImageField(upload_to="organization_logos", null=True, blank=True)
     url = models.URLField(unique=True)
     email = models.EmailField(null=True, blank=True)
-    twitter = models.CharField(max_length=30, null=True, blank=True)
+    twitter = models.CharField(max_length=255, null=True, blank=True)
     facebook = models.URLField(null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
