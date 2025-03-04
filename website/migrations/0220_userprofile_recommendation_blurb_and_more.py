1b829f83b9a6502dac88ebf7be715cce1e9da2e9
Krishna Kaushal <sendittokrishnaa@gmail.com>
2025-03-05 02:10:06 +0530

    Added styles to profile.html

--- website/migrations/0220_userprofile_recommendation_blurb_and_more.py
+++ website/migrations/0220_userprofile_recommendation_blurb_and_more.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("website", "0210_merge_0209_baconsubmission_0209_repo_organization"),
+        ("website", "0219_organization_repos_updated_at"),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
