a84d0a755ea25fec9a79302ca90973f5ccd3eb6f
Sonu <looksonu0007@gmail.com>
2025-03-21 01:07:49 +0530

    Fixed isort issue by removing unnecessary blank line

--- blt/settings.py
+++ blt/settings.py
@@ -1,10 +1,7 @@
 import json
 import os
 import sys
-
 import environ
-
-# Initialize Sentry
 import sentry_sdk
 from django.utils.translation import gettext_lazy as _
 from google.oauth2 import service_account
