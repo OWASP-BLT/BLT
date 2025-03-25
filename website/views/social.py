95de932527c070c06a0fa12be20677928585e4e3
swaparup36 <swaparup36@gmail.com>
2025-03-26 01:25:21 +0530

    paginator added to /social

--- website/views/social.py
+++ website/views/social.py
@@ -1,3 +1,4 @@
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import render
 
 from website.models import Queue
@@ -7,5 +8,21 @@ def queue_social_view(request):
     # Get all queue items ordered by creation date
     queue_items = Queue.objects.all().order_by("-created")
 
-    print("Queue items: ", queue_items[0].image)
-    return render(request, "social.html", {"queue_items": queue_items})
+    # Add pagination - 10 items per page
+    paginator = Paginator(queue_items, 10)
+    page_number = request.GET.get("page", 1)
+    try:
+        page_obj = paginator.page(page_number)
+    except (PageNotAnInteger, EmptyPage):
+        page_obj = paginator.page(1)
+
+        page_obj = paginator.page(1)
+
+    return render(
+        request,
+        "social.html",
+        {
+            "queue_items": page_obj.object_list,
+            "page_obj": page_obj,
+        },
+    )
