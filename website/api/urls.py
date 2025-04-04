39ecb45127c09cbe21daf1f3472fecd7df3cf993
Aditya <132193415+thedvlprguy@users.noreply.github.com>
2025-04-04 23:14:03 +0530

    Create urls.py

---
+++ website/api/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("stats/views", views.view_stats, name="view-stats"),
+    path("github-issues/<int:issue_number>", views.github_issue, name="github-issue"),
+    path("badge/issue/<int:issue_number>", views.issue_badge, name="issue-badge"),
+]
