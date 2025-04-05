ad2645d99d767760bbf4de1051a410862aa2526a
swaparup36 <swaparup36@gmail.com>
2025-04-05 21:37:57 +0530

    test_login_by_email fixed

--- website/tests/test_api.py
+++ website/tests/test_api.py
@@ -129,7 +129,7 @@ class APITests(APITestCase):
         self.token = response.json()["key"]
 
         payload = {
-            "username": self.EMAIL,
+            "email": self.EMAIL,
             "password": self.PASS,
         }
         response = self.client.post(self.login_url, data=payload)
