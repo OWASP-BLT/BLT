dda7538423aa09c4690ecf8cc43ab427f631fdb6
P Nitish <nitishnaik2022@gmail.com>
2025-05-20 00:42:06 +0530

    fix/auto-pay-out-bounty

--- blt/urls.py
+++ blt/urls.py
@@ -51,6 +51,9 @@ from website.views.bitcoin import (
     update_submission_status,
 )
 from website.views.blog import PostCreateView, PostDeleteView, PostDetailView, PostListView, PostUpdateView
+from website.views.bounty import (
+    bounty_payout
+)
 from website.views.company import (
     AddDomainView,
     AddHuntView,
@@ -1095,6 +1098,7 @@ urlpatterns = [
     path("reminder-settings/", reminder_settings, name="reminder_settings"),
     path("send-test-reminder/", send_test_reminder, name="send_test_reminder"),
     path("check_domain_security_txt/", check_domain_security_txt, name="check_domain_security_txt"),
+    path("bounty_payout/", bounty_payout, name="bounty_payout")
 ]
 
 if settings.DEBUG:
