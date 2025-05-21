ec8e5cb8244eadf1791c0881a74c057edeede105
P Nitish <nitishnaik2022@gmail.com>
2025-05-21 21:33:59 +0530

    fix pre-commit

--- website/views/bounty.py
+++ website/views/bounty.py
@@ -7,7 +7,9 @@ from django.conf import settings
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
+
 from website.models import Contributor, GitHubIssue, Repo, UserProfile
+
 logger = logging.getLogger(__name__)
 
 
