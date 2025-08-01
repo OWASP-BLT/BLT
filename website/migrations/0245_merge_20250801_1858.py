4945ba50e0bea66e9f0fc3c6c67a3698bfb24f40
Lucky negi <luckynegi1025@gmail.com>
2025-08-02 00:37:03 +0530

    pre_commit

--- website/migrations/0245_merge_20250801_1858.py
+++ website/migrations/0245_merge_20250801_1858.py
@@ -4,7 +4,6 @@ from django.db import migrations
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ("website", "0244_stakingpool_stakingtransaction_stakingentry"),
         ("website", "0244_tasks_taskcontent"),
