12a83f5c801646cd3a018740ca597e15880f88ec
Sahil Dhillon <sodhillon_b21@it.vjti.ac.in>
2025-08-29 20:24:32 +0530

    Remove config call

--- website/aibot/clients.py
+++ website/aibot/clients.py
@@ -2,11 +2,6 @@ from django.conf import settings
 from google import genai
 from qdrant_client import QdrantClient
 
-from website.aibot.aibot_env import configure_and_validate_settings
-
-configure_and_validate_settings()
-# TODO: Remove settings config call once testing is done
-
 g_client = genai.Client(api_key=settings.GEMINI_API_KEY)
 
 q_client = QdrantClient(host=settings.QDRANT_HOST, port=settings.QDRANT_HTTP_PORT)
