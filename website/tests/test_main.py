9a1df13969d29ef687311e8a0de9ee739f27c15d
Siddharth Bansal <sidd190bansal@gmail.com>
2025-11-25 00:07:50 +0530

    Fix: test_main.py pre-commit error

--- website/tests/test_main.py
+++ website/tests/test_main.py
@@ -338,7 +338,6 @@ class LeaderboardTests(TestCase):
             state="closed",
             created_at=timezone.now(),
             updated_at=timezone.now(),
-            merged_at=timezone.now(),
             url="https://github.com/OWASP-BLT/BLT/pull/1",
             issue_id=1,
         )
@@ -353,7 +352,6 @@ class LeaderboardTests(TestCase):
             state="closed",
             created_at=timezone.now(),
             updated_at=timezone.now(),
-            merged_at=timezone.now(),
             url="https://github.com/OWASP-BLT/BLT/pull/2",
             issue_id=2,
         )
