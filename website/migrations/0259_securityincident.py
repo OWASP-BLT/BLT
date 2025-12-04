563eef36f1890993e944ad082337f930b9d21296
Nachiket Roy <nachiket.roy.2@gmail.com>
2025-12-04 05:07:00 +0000

    migration conflict resolved

--- website/migrations/0259_securityincident.py
+++ website/migrations/0259_securityincident.py
@@ -5,7 +5,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("website", "0258_add_slackchannel_model"),
+        ("website", "0259_add_search_history"),
     ]
 
     operations = [
