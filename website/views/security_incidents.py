587f138d38e387f2b4d4e7a0bc3ac506452381f6
Nachiket Roy <nachiket.roy.2@gmail.com>
2025-12-05 06:34:26 +0000

    fixed form validation

--- website/views/security_incidents.py
+++ website/views/security_incidents.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.db import transaction
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, DetailView, UpdateView
 
@@ -23,13 +24,16 @@ class SecurityIncidentCreateView(LoginRequiredMixin, StaffRequiredMixin, CreateV
         context = super().get_context_data(**kwargs)
 
         obj = getattr(self, "object", None)
-
         if obj and hasattr(obj, "history"):
             context["unique_editor_count"] = obj.history.values("changed_by").distinct().count()
         else:
             context["unique_editor_count"] = 0
 
         return context
+
+    @transaction.atomic
+    def form_valid(self, form):
+        return super().form_valid(form)
 
 
 class SecurityIncidentUpdateView(LoginRequiredMixin, StaffRequiredMixin, UpdateView):
@@ -43,6 +47,10 @@ class SecurityIncidentUpdateView(LoginRequiredMixin, StaffRequiredMixin, UpdateV
         context["form_title"] = "Edit Security Incident"
         return context
 
+    @transaction.atomic
+    def form_valid(self, form):
+        return super().form_valid(form)
+
 
 class SecurityIncidentDetailView(LoginRequiredMixin, DetailView):
     model = SecurityIncident
@@ -50,8 +58,5 @@ class SecurityIncidentDetailView(LoginRequiredMixin, DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        incident = self.object
-
-        # add more metadata later (history, related issues, etc.)
-        context["incident"] = incident
+        context["incident"] = self.object
         return context
