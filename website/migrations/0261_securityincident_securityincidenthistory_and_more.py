3f2b01f766c476efdaf65d3f308f7db6f0feb5e4
Nachiket Roy <nachiket.roy.2@gmail.com>
2025-12-06 12:57:04 +0000

    migration updated

--- website/migrations/0261_securityincident_securityincidenthistory_and_more.py
+++ website/migrations/0261_securityincident_securityincidenthistory_and_more.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("website", "0259_add_search_history"),
+        ("website", "0260_add_username_to_slackbotactivity"),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
