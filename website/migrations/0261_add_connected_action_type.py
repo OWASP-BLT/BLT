97a55da13fb2ba0b0484744a708557817817252d
Siddharth Bansal <sidd190bansal@gmail.com>
2025-12-07 14:50:30 +0530

    Chore: Fix Pre-commit, add handlers as per CodeRabbit

--- website/migrations/0261_add_connected_action_type.py
+++ website/migrations/0261_add_connected_action_type.py
@@ -4,23 +4,22 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('website', '0260_add_username_to_slackbotactivity'),
+        ("website", "0260_add_username_to_slackbotactivity"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='activity',
-            name='action_type',
+            model_name="activity",
+            name="action_type",
             field=models.CharField(
                 max_length=20,
                 choices=[
-                    ('create', 'Created'),
-                    ('update', 'Updated'),
-                    ('delete', 'Deleted'),
-                    ('signup', 'Signed Up'),
-                    ('connected', 'Connected'),
+                    ("create", "Created"),
+                    ("update", "Updated"),
+                    ("delete", "Deleted"),
+                    ("signup", "Signed Up"),
+                    ("connected", "Connected"),
                 ],
             ),
         ),
