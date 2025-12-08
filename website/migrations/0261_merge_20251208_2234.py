4dafcff2e38f469114eb7fec654cc14ad4d38307
swaparup36 <swaparup36@gmail.com>
2025-12-08 22:46:11 +0530

    fix: pre-commit

--- website/migrations/0261_merge_20251208_2234.py
+++ website/migrations/0261_merge_20251208_2234.py
@@ -4,7 +4,6 @@ from django.db import migrations
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ("website", "0260_add_username_to_slackbotactivity"),
         ("website", "0260_merge_20251205_1407"),
