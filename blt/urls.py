a83240e71c49eb2c3aa256f549c874f6f1bfc511
Nachiket Roy <nachiket.roy.2@gmail.com>
2025-12-11 12:26:48 +0000

    proper condition added

--- blt/urls.py
+++ blt/urls.py
@@ -1276,7 +1276,7 @@ if settings.DEBUG:
 
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-if settings.DEBUG:
+if settings.DEBUG and not settings.TESTING:
     urlpatterns += [
         path("silk/", include("silk.urls", namespace="silk")),
     ]
