017cdb8a8dd7a10f7ca0b93d8040278c8db3e09d
armanrayma <armanrayma900@gmail.com>
2025-12-16 16:47:09 +0530

    Fixed PostgreSQL ValueError by loading env variables in manage.py

--- manage.py
+++ manage.py
@@ -2,9 +2,16 @@
 import os
 import sys
 
-if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blt.settings")
 
+def main():
+    
+    from dotenv import load_dotenv
+    load_dotenv()
+
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blt.settings')
     from django.core.management import execute_from_command_line
-
     execute_from_command_line(sys.argv)
+
+
+if __name__ == '__main__':
+    main()
