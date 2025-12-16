d99151526e77838d3567046dc8c825bd6722c863
armanrayma <armanrayma900@gmail.com>
2025-12-16 19:56:42 +0530

    commit message

--- manage.py
+++ manage.py
@@ -9,6 +9,7 @@ def main():
     load_dotenv()
 
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blt.settings")
+
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
