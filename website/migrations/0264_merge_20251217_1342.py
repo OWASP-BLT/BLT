6da02bd9d86a615e82d71ab8efe6405596e14ed9
snigdha choudhury <snigdhachoudhury2023@gmail.com>
2025-12-17 14:59:10 +0530

    django tests

--- website/migrations/0264_merge_20251217_1342.py
+++ website/migrations/0264_merge_20251217_1342.py
@@ -4,7 +4,6 @@ from django.db import migrations
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ("website", "0247_job"),
         ("website", "0263_githubissue_githubissue_pr_merged_idx_and_more"),
