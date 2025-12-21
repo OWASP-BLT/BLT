fdb70762a01a0d2916854ca54b9d5c1938603eac
swaparup36 <swaparup36@gmail.com>
2025-12-21 23:37:20 +0530

    fix: pre-commit

--- website/migrations/0264_merge_20251221_2332.py
+++ website/migrations/0264_merge_20251221_2332.py
@@ -4,7 +4,6 @@ from django.db import migrations
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ("website", "0261_merge_20251208_2234"),
         ("website", "0263_githubissue_githubissue_pr_merged_idx_and_more"),
