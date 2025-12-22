4148f8c99962f1b587851b80ecbe17f23852b55e
Shaz Zahra Zaidi <shazzahra.zaidi@gmail.com>
2025-12-22 19:27:27 +0530

    ruff

--- website/views/education.py
+++ website/views/education.py
@@ -27,7 +27,6 @@ openai.api_key = settings.OPENAI_API_KEY
 
 from django.contrib.auth.models import User
 from django.test import TestCase
-from django.urls import reverse
 
 
 class EducationSubmitViewTests(TestCase):
