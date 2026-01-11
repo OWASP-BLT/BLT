bd3f7271448f760a15e944aa11177de6da624533
Savio Dsouza <saviodsouza8a@gmail.com>
2026-01-11 17:49:28 +0530

    Update forms.py

--- website/forms.py
+++ website/forms.py
@@ -124,7 +124,7 @@ class IssueForm(forms.ModelForm):
 
     class Meta:
         model = Issue
-        fields = ['url', 'description', 'markdown_description', 'label']
+        fields = ['url', 'description', 'markdown_description', 'label', 'domain', 'cve_id']
         widgets = {
             'url': forms.URLInput(attrs={
                 'class': 'w-full px-4 py-2 border border-gray-300 dark:border-gray-600 dark:bg-gray-700 dark:text-gray-100 rounded-lg focus:ring-2 focus:ring-[#e74c3c] focus:border-transparent',
