77ea5909fa6101738f1891c9ef5d5b6642819107
Savio Dsouza <saviodsouza8a@gmail.com>
2026-02-09 17:37:48 +0530

    Update models.py

--- website/models.py
+++ website/models.py
@@ -967,19 +967,19 @@ class UserProfile(models.Model):
     longest_streak = models.IntegerField(default=0)
     last_check_in = models.DateField(null=True, blank=True)
 
-    def avatar(self, size=36):
-    from website.utils import gravatar_url
-    
-    if self.user_avatar:
-        return self.user_avatar.url
-    
-    for account in self.user.socialaccount_set.all().order_by('id'):
-        if "avatar_url" in account.extra_data:
-            return account.extra_data["avatar_url"]
-        elif "picture" in account.extra_data:
-            return account.extra_data["picture"]
-    
-    return gravatar_url(self.user.email)  
+     def avatar(self, size=36):
+         from website.utils import gravatar_url
+      
+         if self.user_avatar:
+            return self.user_avatar.url
+      
+         for account in self.user.socialaccount_set.all().order_by('id'):
+             if "avatar_url" in account.extra_data:              
+                 return account.extra_data["avatar_url"]         
+             elif "picture" in account.extra_data:               
+                  return account.extra_data["picture"]            
+      
+          return gravatar_url(self.user.email)                    
     
     def __unicode__(self):
         return self.user.email
