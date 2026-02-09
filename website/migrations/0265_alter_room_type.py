10894ba5c831f8b75a25d47fd7cc7d01394fc84b
Sakshee Suman <187769872+e-esakman@users.noreply.github.com>
2026-02-09 15:13:08 +0530

    fix:migration-file-number

---
+++ website/migrations/0265_alter_room_type.py
@@ -0,0 +1,35 @@
+from django.db import migrations, models
+
+
+def forwards(apps, schema_editor):
+    Room = apps.get_model("website", "Room")
+    Room.objects.filter(type="org").update(type="organization")
+
+
+def backwards(apps, schema_editor):
+    Room = apps.get_model("website", "Room")
+    Room.objects.filter(type="organization").update(type="org")
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("website", "0264_remove_forum_models"), 
+    ]
+
+    operations = [
+        migrations.RunPython(forwards, backwards),
+        migrations.AlterField(
+            model_name="room",
+            name="type",
+            field=models.CharField(
+                choices=[
+                    ("project", "Project"),
+                    ("bug", "Bug"),
+                    ("organization", "Organization"),
+                    ("custom", "Custom"),
+                ],
+                max_length=20,
+            ),
+        ),
+    ]
