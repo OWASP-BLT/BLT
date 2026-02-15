f04c69c0cf14c0afe610b76c2c892d9882a66cef
Nachiket Roy <nachiket.roy.2@gmail.com>
2026-02-15 22:58:31 +0300

    fixed typo

--- website/tests/test_issue_htmx.py
+++ website/tests/test_issue_htmx.py
@@ -93,7 +93,7 @@ class IssueHTMXTests(TestCase):
 
     def test_like_rate_limit(self):
         """Test that like action is rate limited"""
-        # Make 10 requests (should succeed)
+        # Make 60 requests (should succeed)
         for _ in range(60):
             response = self.client.post(
                 reverse("like_issue", kwargs={"issue_pk": self.issue.pk}),
@@ -101,7 +101,7 @@ class IssueHTMXTests(TestCase):
             )
             self.assertEqual(response.status_code, 200)
 
-        # 21st request should be rate limited
+        # 61st request should be rate limited
         response = self.client.post(
             reverse("like_issue", kwargs={"issue_pk": self.issue.pk}),
             HTTP_HX_REQUEST="true",
