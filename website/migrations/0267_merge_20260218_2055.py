2b39cbe0e1714e8cc49fd569d9f6752a92cbb748
Aditya Gupta <adityagupta@Adityas-MacBook-Pro-2.local>
2026-02-19 02:47:45 +0530

    Fix ruff formatting

--- website/migrations/0267_merge_20260218_2055.py
+++ website/migrations/0267_merge_20260218_2055.py
@@ -4,7 +4,6 @@ from django.db import migrations
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ("website", "0266_add_cve_indexes"),
         ("website", "0266_alter_usertasksubmission_proof_url_delete_post"),
