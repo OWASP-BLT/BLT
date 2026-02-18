cbb8b556dc23b95b17cbb4d03a82d784052fc8de
Jayant Malvi <cs24b077@smail.iitm.ac.in>
2026-02-18 10:19:08 +0530

    Initial Commit

--- blt/settings.py
+++ blt/settings.py
@@ -369,7 +369,9 @@ else:
         }
 
 ACCOUNT_SIGNUP_FIELDS = ["email*", "username*", "password1*", "password2*"]
-ACCOUNT_EMAIL_VERIFICATION = "mandatory"
+ACCOUNT_EMAIL_VERIFICATION = "optional"
+ACCOUNT_AUTHENTICATION_METHOD = "username_email"
+ACCOUNT_UNIQUE_EMAIL = True
 ACCOUNT_FORMS = {"signup": "website.forms.SignupFormWithCaptcha"}
 # Security: Do not send emails to unknown accounts during password reset
 # This prevents account enumeration attacks
