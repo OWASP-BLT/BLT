4c98e02f6fd9ecafae8dd6129e529fa267684709
Jayant Malvi <cs24b077@smail.iitm.ac.in>
2026-02-18 10:45:40 +0530

    Added email_required to fix this all-auth error

--- blt/settings.py
+++ blt/settings.py
@@ -370,6 +370,7 @@ else:
 
 ACCOUNT_SIGNUP_FIELDS = ["email*", "username*", "password1*", "password2*"]
 ACCOUNT_EMAIL_VERIFICATION = "mandatory"
+ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_LOGIN_METHODS = {"username", "email"}
 ACCOUNT_UNIQUE_EMAIL = True
 ACCOUNT_FORMS = {"signup": "website.forms.SignupFormWithCaptcha"}
