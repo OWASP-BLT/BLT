fd34111df0680de3d9dbf7e4dea9f40e2162e3e0
Sakshee Suman <187769872+e-esakman@users.noreply.github.com>
2026-02-20 16:30:40 +0530

    fix pre-commit

--- website/migrations/0269_alter_project_freshness_alter_room_type.py
+++ website/migrations/0269_alter_project_freshness_alter_room_type.py
@@ -5,7 +5,6 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ("website", "0268_project_freshness_repo_participation_stats"),
     ]
