f6509b6248d691320f6bffe54aa7f4d7333c05ca
Yashvardhan Vashistha <vashisthayashvardhan@gmail.com>
2026-02-23 02:07:37 +0530

    test(blog): isolate MEDIA_ROOT and use valid image bytes

--- website/tests/test_blog.py
+++ website/tests/test_blog.py
@@ -1,12 +1,25 @@
-from django.core.files.uploadedfile import SimpleUploadedFile
-from django.test import TestCase
+import shutil
+import tempfile
+
+from django.conf import settings
 from django.contrib.auth.models import User
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.test import TestCase, override_settings
 from django.urls import reverse
 
 from website.models import Post
 
 
+TEMP_MEDIA_ROOT = tempfile.mkdtemp()
+
+
+@override_settings(MEDIA_ROOT=TEMP_MEDIA_ROOT)
 class PostDetailViewTests(TestCase):
+    @classmethod
+    def tearDownClass(cls):
+        super().tearDownClass()
+        shutil.rmtree(TEMP_MEDIA_ROOT, ignore_errors=True)
+
     def setUp(self):
         self.user = User.objects.create_user(
             username="author",
@@ -14,11 +27,16 @@ class PostDetailViewTests(TestCase):
             email="author@example.com",
         )
 
-        # Create dummy image
+        # Minimal valid 1x1 PNG image
         self.image = SimpleUploadedFile(
-            "test.jpg",
-            b"file_content",
-            content_type="image/jpeg",
+            "test.png",
+            (
+                b"\x89PNG\r\n\x1a\n\x00\x00\x00\rIHDR\x00\x00\x00\x01"
+                b"\x00\x00\x00\x01\x08\x02\x00\x00\x00\x90wS\xde"
+                b"\x00\x00\x00\nIDATx\xdac\xf8\xff\xff?\x00\x05\xfe\x02\xfeA"
+                b"\xe2\x1c\x00\x00\x00\x00IEND\xaeB`\x82"
+            ),
+            content_type="image/png",
         )
 
         self.post = Post.objects.create(
