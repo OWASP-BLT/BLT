ef6b004028ce48c41ba1c9608bb686366cfed6ae
Nachiket Roy <nachiket.roy.2@gmail.com>
2026-02-23 19:09:39 +0300

    heruko specific email field

--- blt/settings.py
+++ blt/settings.py
@@ -287,6 +287,7 @@ REPORT_EMAIL_PASSWORD = os.environ.get("REPORT_PASSWORD", "blank")
 
 if "DYNO" in os.environ:  # for Heroku
     DEBUG = False
+    EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
     EMAIL_HOST = "smtp.sendgrid.net"
     EMAIL_HOST_USER = os.environ.get("SENDGRID_USERNAME", "blank")
     EMAIL_HOST_PASSWORD = os.environ.get("SENDGRID_PASSWORD", "blank")
