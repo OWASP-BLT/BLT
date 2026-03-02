a6ecbc648dff42877b5314c56e20f48b7b3be320
snigdha <snigdhachoudhury2023@gmail.com>
2026-03-02 23:48:06 +0530

    Fix migration conflict and remove console logs

--- website/migrations/0272_add_organization_social_stats.py
+++ website/migrations/0272_add_organization_social_stats.py
@@ -5,7 +5,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("website", "0270_githubcomment"),
+        ("website", "0271"),
     ]
 
     operations = [
